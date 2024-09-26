2c6ec97842da94ba234d191d63a8b12429087084
nahid-sebpo <95094891+nahid-sebpo@users.noreply.github.com>
2024-09-27 05:00:56 +0600

    project setup

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import postsRouter from './routes/posts.js';
+import connectDB from './config/db.js';
+import cors from 'cors';
+import compression from 'compression';
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(compression());
+
+// Connect to database
+connectDB();
+
+// API route
+app.use('/api/posts', postsRouter);
+
+app.get('/', (req, res) => {
+  res.send('Yay!! Backend of wanderlust app is now accessible ');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export default app;
